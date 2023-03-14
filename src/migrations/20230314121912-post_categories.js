ef45a5ffc0567d12df48cf662276e9ac66780a02
Ester Viana Verza <ester.av@hotmail.com>
2023-03-14 12:37:30 -0300

    requisito 1

---
+++ src/migrations/20230314121912-post_categories.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', {
+      post_id: {
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'blog_posts',
+          key: 'id'
+        }
+      },
+      category_id: {
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'categories',
+          key: 'id'
+        }
+      }
+    }, {
+      underscored: true,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts_categories')
+  }
+};
