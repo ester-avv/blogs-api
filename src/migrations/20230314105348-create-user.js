22da859eaf4284f41a93ed215d0ef7e836d09dc1
Ester Viana Verza <ester.av@hotmail.com>
2023-03-14 09:14:04 -0300

    teste

---
+++ src/migrations/20230314105348-create-user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      display_name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      image: {
+        allowNull: true,
+        type: Sequelize.STRING
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users')
+  }
+};
